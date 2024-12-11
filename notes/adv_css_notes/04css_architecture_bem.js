d16616f45579cba14ec76f5e873b63a38844b879
Matt Frutchey <mjfru201@gmail.com>
2024-12-11 00:26:07 -0500

    add brief notes on css architecture and best practices

---
+++ notes/adv_css_notes/04css_architecture_bem.js
@@ -0,0 +1,20 @@
+/*
+! The Think - Build - Architect Mindset
+* Think about the layout of your webpage or web app before writing code.
+  ? Component-driven design
+  - Modular building blocks that make up interfaces
+  - Held together by the layout of the page
+  - Re-usable across a project and even between different projects
+  - Independent, allowing us to use them anywhere on the page.
+
+* Build your layout in HTML and CSS with consistent structure for naming classes.
+  ? BEM - Block Element Modifier
+  - A BLOCK is a standalone component that is meaningful on its own
+  - An ELEMENT is part of a block that has no meaning on its own
+  - A MODIFIER is a different version of a block or an element
+
+* Create a logical architecture for your CSS with files and folders
+  ? The 7-1 Pattern
+  - 7 different folders for partial Sass files and 1 main Sass file to import all other files into a compiled CSS style sheet.
+  base/ components/ layout/ pages/ themes/ abstracts/ vendors/
+*/
